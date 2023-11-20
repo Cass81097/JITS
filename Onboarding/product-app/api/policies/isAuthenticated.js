8532b3c9919154838dfa1f8fd40d237edf662805
Hoang Anh <hoanganh81097@gmail.com>
2023-11-20 21:50:30 +0700

    jwt

--- Onboarding/product-app/api/policies/isAuthenticated.js
+++ Onboarding/product-app/api/policies/isAuthenticated.js
@@ -2,14 +2,18 @@ let jwt = require('jsonwebtoken');
 
 module.exports = async function (req, res, next) {
   if (!req.headers || !req.headers.authorization) {
-    return res.unauthorized({ message: 'No authorization header found.' });
+    // return res.unauthorized({ message: 'No authorization header found.' });
+    return res.status(401).send({ message: 'No authorization header found.' });
+
   }
 
   let token = req.headers.authorization.split(' ')[1]; 
 
   jwt.verify(token, 'YOUR_SECRET_KEY', function(err, decoded) {
     if (err) {
-      return res.unauthorized({ message: 'Invalid Token!' });
+      // return res.unauthorized({ message: 'Invalid Token!' });
+      return res.status(401).send({ message: 'Invalid Token!'});
+
     }
     req.user = decoded; 
     return next();
